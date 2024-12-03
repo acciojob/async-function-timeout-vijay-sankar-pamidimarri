c42b71bb5a9a82a0a37f43326b6be675f09d703a
Pamidimarri vijay sankar <sankerpamidimarri@gmail.com>
2024-12-03 15:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let output=document.querySelector("span");
+
+document.querySelector("button").addEventListener("click",async ()=>{
+    let text=document.querySelector("#text").value;
+    let time=document.querySelector("#delay").value;
+    let p=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+       resolve(text);
+    },Number(time));
+  });
+  let k1=await p;
+  output.textContent=k1;
+  document.querySelector("#text").value="";
+  document.querySelector("#delay").value="";
+})
